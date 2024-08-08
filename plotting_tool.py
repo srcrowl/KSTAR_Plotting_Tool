88ba34f531d43f3bae99f255cc1c324fb5b82c10
Sam Crowl <51138150+srcrowl@users.noreply.github.com>
2024-08-08 17:02:19 +0000

    Added ability to select specific samples and save different file types

--- plotting_tool.py
+++ plotting_tool.py
@@ -5,9 +5,14 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from kstar import plot
 
-def kstar_plot(activities, fpr, colormap, background_color = 'white', sig_restrict = False, kinases_to_drop = None, binary_sig = True, sort_kinases = True, sort_samples = True, binary_evidence = None, evidence_type = 'dots', context = None, figsize = (4,10)):
+def kstar_plot(activities, fpr, colormap, background_color = 'white', samples = None, x_label_dict = None, sig_restrict = False, kinases_to_drop = None, binary_sig = True, sort_kinases = True, sort_samples = True, binary_evidence = None, evidence_type = 'dots', context = None, phosphotype = None, fontsize = 10, figsize = (4,10)):
     #log transform activities
     log_results = -np.log10(activities)
+
+    if samples is not None:
+        log_results = log_results[samples]
+        fpr = fpr[samples]
+
     if binary_evidence is not None:
         include_evidence = True
     else:
@@ -44,7 +49,8 @@ def kstar_plot(activities, fpr, colormap, background_color = 'white', sig_restri
                 legend_title = '-log10(p-value)',
                 colormap = colormap,
                 facecolor=background_color,
-                binary_sig = binary_sig)
+                binary_sig = binary_sig,
+                x_label_dict=x_label_dict)
 
     if sig_restrict:
         dots.drop_kinases_with_no_significance()
@@ -68,7 +74,9 @@ def kstar_plot(activities, fpr, colormap, background_color = 'white', sig_restri
         ax = axes[2,1] if sort_samples and sort_kinases else axes[2] if sort_samples else axes[1]
         ax.set_yticks([])
         dots.evidence_count(ax = ax, binary_evidence = binary_evidence, plot_type = evidence_type,
-                    include_recommendations = True, phospho_type = 'Y')
+                    include_recommendations = True, phospho_type = phosphotype)
+        ax.tick_params(labelsize = fontsize)
+        ax.tick_params(axis = 'x', rotation = 90)
         if sort_kinases:
             axes[nrows-1,0].axis('off')
 
@@ -77,7 +85,8 @@ def kstar_plot(activities, fpr, colormap, background_color = 'white', sig_restri
     #generate the dotplot
     ax = axes[1,1] if sort_samples and sort_kinases else axes[1] if sort_samples or sort_kinases else axes
     dots.dotplot(ax = ax)
-    plt.xticks(rotation = 90)
+    ax.tick_params(axis = 'y', labelsize = fontsize)
+    ax.tick_params(axis = 'x', labelsize = fontsize, rotation = 90)
     st.pyplot(fig)
     return fig
 
@@ -100,13 +109,14 @@ st.header('Plot')
 
 #plot_type = st.selectbox('Select Plot Type:', ['Dotplot', 'Barplot'], key = 'plot_type')
 
-t1, t2, t3, t4 = st.tabs(['Figure Parameters', 'Filter Kinases', 'Sort Kinases/Samples', 'Add Additional Context'])
+t1, t2, t3, t4 = st.tabs(['Figure Parameters', 'Filter Kinases and Samples', 'Sort Kinases/Samples', 'Add Additional Context'])
 with t1:
     #establish figure size
     st.markdown('*Figure Size*')
     col1, col2 = st.columns(2)
     fwidth = col1.number_input('Width (in)', min_value=1, max_value=20, value=4, key = 'width')
     fheight = col2.number_input('Height (in)', min_value=1, max_value=20, value=10, key = 'height')
+    fontsize = st.number_input('Fontsize', min_value = 6, max_value = 18, value = 10, key = 'fontsize')
 
     
     st.markdown('*Color Scheme*')
@@ -124,8 +134,9 @@ with t1:
 
 
 with t2:
-    sig_restrict = st.checkbox('Restrict to kinases with significance in at least one sample', value = False, key = 'sig_restrict')
     if 'activities' in locals():
+        st.subheader('Filter Kinases')
+        sig_restrict = st.checkbox('Restrict to kinases with significance in at least one sample', value = False, key = 'sig_restrict')
         #ask if user wants to drop any kinases
         manual_kinase_edit = st.selectbox('Manually edit kinases in plot:', ['No', 'Remove Specific Kinases', 'Select Specific Kinases'])
         if manual_kinase_edit == 'Remove Specific Kinases':
@@ -135,6 +146,19 @@ with t2:
             kinases_to_drop = [kin for kin in activities.index if kin not in kinases_to_keep]
         else:
             kinases_to_drop = None
+
+        st.subheader('Filter Samples')
+        samples_to_plot = st.multiselect('Manually select samples to plot:', options = activities.columns, default = list(activities.columns), key = 'samples_to_plot')
+        
+        #rename 
+        sample_rename = st.checkbox('Change the xlabel for each sample',value = False, key = 'sample_rename')
+        if sample_rename:
+            x_label_dict = {}
+            for samp in samples_to_plot:
+                x_label_dict[samp] = st.text_input(label = samp, value = samp, key = samp + '_label')
+        else:
+            x_label_dict = None
+
 
 with t3:
     sort_kinases = st.radio('Sort Kinases:', ['No Sorting', 'By Activity', 'By Hierarchical Clustering'], key = 'sort_kinases', horizontal = True)
@@ -161,7 +185,8 @@ with t4:
     st.subheader('Add evidence size used for each sample')
     include_evidence = st.checkbox('Include Evidence Size for Each Sample', key = 'include_evidence')
     if include_evidence:
-        evidence_type = st.radio("How to display evidence size:", ['dots','bars'])
+        phosphotype = st.radio("Type of kinase being plotted:", ['Y', 'ST'], horizontal = True)
+        evidence_type = st.radio("How to display evidence size:", ['dots','bars'], horizontal = True)
         evidence_upload = st.file_uploader('Upload binary evidence file:', type = 'tsv', key = 'binary_evidence', help = 'This should be the file labeled "..._evidence_binary.tsv"')
         if evidence_upload is not None:
             binary_evidence = pd.read_csv(evidence_upload, sep = '\t', index_col = 0)
@@ -173,27 +198,40 @@ with t4:
         context = None
 
 
-plot_now = st.button('Plot')
+st.subheader('Construct DotPlot')
+plot_now = st.checkbox('Show Plot', value = False, key = 'show_plot')
 if plot_now:
     if 'activities' in locals() and 'fpr' in locals():
         if include_evidence and 'binary_evidence' in locals() and add_context and 'context' in locals():
-            fig = kstar_plot(activities, fpr, colormap = {0: noactivity_color, 1: activity_color}, background_color = background_color, figsize = (fwidth, fheight), sig_restrict = sig_restrict, kinases_to_drop = kinases_to_drop, binary_sig = binary_sig, sort_kinases = sort_kinases, sort_samples = sort_samples, binary_evidence=binary_evidence, context = context)
+            fig = kstar_plot(activities, fpr, colormap = {0: noactivity_color, 1: activity_color}, background_color = background_color, figsize = (fwidth, fheight), sig_restrict = sig_restrict, kinases_to_drop = kinases_to_drop, binary_sig = binary_sig, sort_kinases = sort_kinases, sort_samples = sort_samples, binary_evidence=binary_evidence, evidence_type = evidence_type, phosphotype = phosphotype, fontsize = fontsize, context = context, samples = samples_to_plot, x_label_dict=x_label_dict)
         elif include_evidence and 'binary_evidence' not in locals() and add_context and 'context' not in locals():
             st.error('Please upload binary evidence file and context file')
         elif include_evidence and 'binary_evidence' in locals() and add_context and 'context' not in locals():
             fig = st.error('Please upload context file')
         elif include_evidence and 'binary_evidence' in locals():
-            fig = kstar_plot(activities, fpr, colormap = {0: noactivity_color, 1: activity_color}, background_color = background_color, figsize = (fwidth, fheight), sig_restrict = sig_restrict, kinases_to_drop = kinases_to_drop, binary_sig = binary_sig, sort_kinases = sort_kinases, sort_samples = sort_samples, binary_evidence=binary_evidence)
+            fig = kstar_plot(activities, fpr, colormap = {0: noactivity_color, 1: activity_color}, background_color = background_color, figsize = (fwidth, fheight), sig_restrict = sig_restrict, kinases_to_drop = kinases_to_drop, binary_sig = binary_sig, sort_kinases = sort_kinases, sort_samples = sort_samples, binary_evidence=binary_evidence, evidence_type = evidence_type, phosphotype=phosphotype, fontsize = fontsize, samples = samples_to_plot, x_label_dict = x_label_dict)
         elif add_context and 'context' in locals():
-            fig = kstar_plot(activities, fpr, colormap = {0: noactivity_color, 1: activity_color}, background_color = background_color, figsize = (fwidth, fheight), sig_restrict = sig_restrict, kinases_to_drop = kinases_to_drop, binary_sig = binary_sig, sort_kinases = sort_kinases, sort_samples = sort_samples, context = context)
+            fig = kstar_plot(activities, fpr, colormap = {0: noactivity_color, 1: activity_color}, background_color = background_color, figsize = (fwidth, fheight), sig_restrict = sig_restrict, kinases_to_drop = kinases_to_drop, binary_sig = binary_sig, sort_kinases = sort_kinases, sort_samples = sort_samples, context = context, fontsize = fontsize, samples = samples_to_plot, x_label_dict = x_label_dict)
         elif include_evidence and 'binary_evidence' not in locals():
             st.error('Please upload binary evidence file')
         else:
-            fig = kstar_plot(activities, fpr, colormap = {0: noactivity_color, 1: activity_color}, background_color = background_color, figsize = (fwidth, fheight), sig_restrict = sig_restrict, kinases_to_drop = kinases_to_drop, binary_sig = binary_sig, sort_kinases = sort_kinases, sort_samples = sort_samples)
+            fig = kstar_plot(activities, fpr, colormap = {0: noactivity_color, 1: activity_color}, background_color = background_color, figsize = (fwidth, fheight), sig_restrict = sig_restrict, kinases_to_drop = kinases_to_drop, binary_sig = binary_sig, sort_kinases = sort_kinases, sort_samples = sort_samples, fontsize = fontsize, samples = samples_to_plot, x_label_dict = x_label_dict)
     else:
         st.error('Please upload both files')
-    
-    file_name = 'KSTAR_dotplot.png'
-    plt.savefig(file_name, format = 'png', bbox_inches = 'tight')
+
+        
+    file_name = st.text_input('Figure name:', value = 'KSTAR_dotplot')
+    extension = st.radio('Type of file to save as:', ['.png','.jpg', '.pdf', '.svg'], horizontal = True)
+    itype = extension.split('.')[1]
+    file_name = file_name + extension
+    plt.savefig(file_name,  bbox_inches = 'tight')
     with open(file_name, 'rb') as img:
-        st.download_button(label = 'Download Figure', data = img, file_name = file_name, mime='image/png')
+        st.download_button(label = 'Download Figure', data = img, file_name = file_name, mime='image/'+itype)
+    
+    #file_name = st.text_input('Figure name:', value = 'KSTAR_dotplot')
+    #extension = st.radio('Type of file to save as:', ['.png','.jpg', '.pdf', '.svg'], horizontal = True)
+    #st.write(file_name + extension)
+    #st.write(extension.strip('.'))
+    #plt.savefig(file_name + extension, format = extension.strip('.'), bbox_inches = 'tight')
+    #with open(file_name+extension, 'rb') as img:
+    #    st.download_button(label = 'Download Figure', data = img, file_name = file_name, mime='image/png')
